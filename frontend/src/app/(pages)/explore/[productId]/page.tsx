48fa124bd9947e5edcc89434a92a3f88b5c82703
Moonlight-12 <sadikingeraldo@gmail.com>
2025-08-01 15:56:30 +1000

    Update page.tsx

--- frontend/src/app/(pages)/explore/[productId]/page.tsx
+++ frontend/src/app/(pages)/explore/[productId]/page.tsx
@@ -1,9 +1,9 @@
-export default function ProductDetails({params}: {params: {productId: string}}) {
+export default async function ProductDetails(props: {params: Promise<{productId: string}>}) {
+  const params = await props.params;
   return (
     <div>
       <h1 className="text-4xl font-bold mb-4">Product Details for {params.productId}</h1>
       {/* Additional product details can be added here */}
     </div>
   );
-
 }
